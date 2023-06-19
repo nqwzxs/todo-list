e33e4aea0dc0240bc43507986ffa86955ecfb4cf
nqwzxs <nqwzxs@gmail.com>
2023-06-19 10:36:37 +0500

    Add todo form and todo grid functions

--- src/index.js
+++ src/index.js
@@ -23,10 +23,10 @@ window.createProject = function(name) {
   projects.push(new Project(name));
 }
 
-window.createTodo = function(title, description, dueDate, priority, project) {
+window.createTodo = function(title, description, dueDate, priority, project, isCompleted) {
   if (selectedProject) project = selectedProject;
   
-  const todo = new Todo(title, description, dueDate, priority, project);
+  const todo = new Todo(title, description, dueDate, priority, project, isCompleted);
   todos.push(todo);
 }
 
@@ -36,4 +36,45 @@ window.selectProject = function(name) {
 
 window.deselectProject = function() {
   selectedProject = null;
+}
+
+window.addTodoForm = document.querySelector('.add-todo-form');
+
+addTodoForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  let title = document.getElementById('title');
+  let description = document.getElementById('description');
+  let dueDate = document.getElementById('due-date');
+
+  const priorities = document.getElementsByName('priority');
+  let priority = Array.from(priorities).find(radio => radio.checked);
+
+  let project = document.getElementById('project');
+  let isCompleted = document.getElementById('is-completed');
+
+  createTodo(title.value, description.value, dueDate.value, priority.value, project.value, isCompleted.checked);
+});
+
+window.todosGrid = document.querySelector('.todos-grid');
+
+window.createTodoCard = function (todo) {
+  const todoCard = document.createElement('div');
+
+  const title = document.createElement('div');
+  title.textContent = todo.title;
+  todoCard.appendChild(title);
+
+  const description = document.createElement('div');
+  description.textContent = todo.description;
+  todoCard.appendChild(description);
+
+  return todoCard;
+}
+
+window.updateTodosGrid = function() {
+  todos.forEach(todo => {
+    const todoCard = createTodoCard(todo);
+    todosGrid.appendChild(todoCard);
+  });
 }
